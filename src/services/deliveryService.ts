8306919ecfa97c25dbc02751518cd38a004dd8a7
Leonardo Rodrigues <59567995+leorjr@users.noreply.github.com>
2024-06-24 16:42:13 -0300

    fix: ajustes de base url

--- src/services/deliveryService.ts
+++ src/services/deliveryService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { CreateEntregaRequest, Entrega } from '../types/delivery';
 
-const API_URL = 'http://localhost:3000/api'; // Certifique-se de que esta URL está correta
+const API_URL = 'http://ec2-3-135-194-155.us-east-2.compute.amazonaws.com/api';
 
 interface GetDeliveriesResponse {
   success: boolean;
