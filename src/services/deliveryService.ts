df1123c4bfe625c92d92e3bc7dcc9cd0993be916
Leonardo Rodrigues <59567995+leorjr@users.noreply.github.com>
2024-06-24 18:35:25 -0300

    fix: ajustes de deploy

--- src/services/deliveryService.ts
+++ src/services/deliveryService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { CreateEntregaRequest, Entrega } from '../types/delivery';
 
-const API_URL = 'http://ec2-3-135-194-155.us-east-2.compute.amazonaws.com/api';
+const API_URL = 'https://3.135.194.155/api';
 
 interface GetDeliveriesResponse {
   success: boolean;
